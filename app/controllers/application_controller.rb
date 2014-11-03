590aff5dd66ca9cc321254427e79913badec7a36
advishnuprasad <advishnuprasad@gmail.com>
2014-11-03 22:48:05 +0530

    Allow additional parameter to signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :update_sanitized_params, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
     root_path
+  end
+
+  def update_sanitized_params
+    devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:phone_no, :building_id, :email, :password, :password_confirmation)}
   end
 end
